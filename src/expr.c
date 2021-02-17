1c0ec50ac2420c16c4df89c92e30c19fec7d6a07
Claudio Daffra <daffra.claudio@gmail.com>
2021-02-17 20:10:56 +0100

    add & | ^ && || ^^

--- src/expr.c
+++ src/expr.c
@@ -512,19 +512,18 @@ node_t* parserOpRelLG( pparser_t this )
  return left ;
 }
 
-// ................................................... EXPR[7]	 ==/?= !=/^^
+// ................................................... EXPR[7]	 ==/?= !=
 
 node_t* parserOpRelEqNe( pparser_t this )
 {
     node_t *left=NULL;
 
-    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserOpRelLG\n" );
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserOpRelEqNe\n" );
 
     left=parserOpRelLG(this);
 
     while (	this->lexer->sym == sym_eq
 		||	this->lexer->sym == sym_ne
-		||	this->lexer->sym == sym_xor
     )
     {
         token_t tSave ;
@@ -544,6 +543,179 @@ node_t* parserOpRelEqNe( pparser_t this )
  return left ;
 }
 
+// ................................................... EXPR[8]	 &
+
+node_t* parserBitAnd( pparser_t this )
+{
+    node_t *left=NULL;
+
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserBitAnd\n" );
+
+    left=parserOpRelEqNe(this);
+
+    while (	this->lexer->sym == sym_bitAnd )
+    {
+        token_t tSave ;
+        parserSaveToken( &tSave , this ) ;
+   
+        node_t *right=NULL;
+
+        parserGetToken(this);
+
+        right=parserOpRelEqNe(this);
+
+        left = astMakeNodeBinOP( this->ast,this->lexer,tSave.sym , right,left ) ;
+
+        parserRestoreToken( left , tSave ) ;
+    } ;
+
+ return left ;
+}
+
+// ................................................... EXPR[9]	 |
+
+node_t* parserBitOr( pparser_t this )
+{
+    node_t *left=NULL;
+
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserBitOr\n" );
+
+    left=parserBitAnd(this);
+
+    while (	this->lexer->sym == sym_bitOr )
+    {
+        token_t tSave ;
+        parserSaveToken( &tSave , this ) ;
+   
+        node_t *right=NULL;
+
+        parserGetToken(this);
+
+        right=parserBitAnd(this);
+
+        left = astMakeNodeBinOP( this->ast,this->lexer,tSave.sym , right,left ) ;
+
+        parserRestoreToken( left , tSave ) ;
+    } ;
+
+ return left ;
+}
+
+// ................................................... EXPR[10]	 ^
+
+node_t* parserBitXor( pparser_t this )
+{
+    node_t *left=NULL;
+
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserBitXor\n" );
+
+    left=parserBitOr(this);
+
+    while (	this->lexer->sym == sym_bitXor )
+    {
+        token_t tSave ;
+        parserSaveToken( &tSave , this ) ;
+   
+        node_t *right=NULL;
+
+        parserGetToken(this);
+
+        right=parserBitOr(this);
+
+        left = astMakeNodeBinOP( this->ast,this->lexer,tSave.sym , right,left ) ;
+
+        parserRestoreToken( left , tSave ) ;
+    } ;
+
+ return left ;
+}
+
+// ................................................... EXPR[11]	 &&
+
+node_t* parserAnd( pparser_t this )
+{
+    node_t *left=NULL;
+
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserAnd\n" );
+
+    left=parserBitXor(this);
+
+    while (	this->lexer->sym == sym_and )
+    {
+        token_t tSave ;
+        parserSaveToken( &tSave , this ) ;
+   
+        node_t *right=NULL;
+
+        parserGetToken(this);
+
+        right=parserBitXor(this);
+
+        left = astMakeNodeBinOP( this->ast,this->lexer,tSave.sym , right,left ) ;
+
+        parserRestoreToken( left , tSave ) ;
+    } ;
+
+ return left ;
+}
+
+// ................................................... EXPR[12]	 ||
+
+node_t* parserOr( pparser_t this )
+{
+    node_t *left=NULL;
+
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserOr\n" );
+
+    left=parserAnd(this);
+
+    while (	this->lexer->sym == sym_or )
+    {
+        token_t tSave ;
+        parserSaveToken( &tSave , this ) ;
+   
+        node_t *right=NULL;
+
+        parserGetToken(this);
+
+        right=parserAnd(this);
+
+        left = astMakeNodeBinOP( this->ast,this->lexer,tSave.sym , right,left ) ;
+
+        parserRestoreToken( left , tSave ) ;
+    } ;
+
+ return left ;
+}
+
+// ................................................... EXPR[12b]	^^ precedence less than != even if is the same thing
+
+node_t* parserXor( pparser_t this )
+{
+    node_t *left=NULL;
+
+    if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserXor\n" );
+
+    left=parserOr(this);
+
+    while (	this->lexer->sym == sym_xor )
+    {
+        token_t tSave ;
+        parserSaveToken( &tSave , this ) ;
+   
+        node_t *right=NULL;
+
+        parserGetToken(this);
+
+        right=parserOr(this);
+
+        left = astMakeNodeBinOP( this->ast,this->lexer,tSave.sym , right,left ) ;
+
+        parserRestoreToken( left , tSave ) ;
+    } ;
+
+ return left ;
+}
 
 // ................................................... EXPR[14]	Assign := += -= *= /= %= <<= >>= &= |= ^=
 
@@ -553,7 +725,7 @@ node_t* parserAssign( pparser_t this )
 
     if ( this->fDebug ) fwprintf ( this->pFileOutputParser , L"parserAssign\n" );
 
-    left=parserOpRelEqNe(this);
+    left=parserXor(this);
 
     while ( this->lexer->sym == sym_assign 
 		||	this->lexer->sym == sym_addEq
@@ -574,7 +746,7 @@ node_t* parserAssign( pparser_t this )
 
         parserGetToken(this);
 
-        right=parserOpRelEqNe(this);
+        right=parserXor(this);
 
         left = astMakeNodeAssign( this->ast,this->lexer,left,right ) ; // invertiti
 
